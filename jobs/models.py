19b7acbb66fb296dc0b51423b15535024dda62ba
ConorClery <cleryc@tcd.ie>
2019-03-16 14:52:24 +0000

    Fleshed out job structure, creation not working yet though

--- jobs/models.py
+++ jobs/models.py
@@ -2,8 +2,25 @@ from django.db import models
 from django.db import models
 from django.core.validators import RegexValidator
 from django.contrib.postgres.fields import ArrayField
-MAX_JOB_SWIPES_ALLOWED = 20
+MAX_JOB_SWIPES_ALLOWED = 50
 
+class Tag(models.Model):
+	tag = models.CharField(max_length=64, unique=True)
+
+class Details(models.Model):
+    job_name = models.CharField(max_length=100)
+    date = models.DateField()
+    time_from = models.TimeField()
+    time_to = models.TimeField()
+    duration = models.DurationField()
+    pay_per_hour = models.DecimalField(decimal_places=2, max_digits=12)
+    tags = models.ManyToManyField(Tag, related_name='job_tags')
+    address = models.TextField()
+    description = models.TextField()
+    required_experience = models.TextField()
 
 class Job(models.Model):
     name = models.CharField(max_length=100)
+    employer_id = models.PositiveIntegerField()
+    def __str__(self):
+        return '%s'%(self.name)
--- jobs/serializers.py
+++ jobs/serializers.py
@@ -4,14 +4,44 @@ from . import models
 class JobsListSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Job
-        fields = ('id', 'name')
+        fields = ('id', 'employer_id', 'details')
 
 class WorkerUpdateSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Job
         fields = ('name',)
 
+class TagsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Tag
+        fields = ('tag',)
+
+class DetailsSerializer(serializers.ModelSerializer):
+    tags = TagsSerializer(many=True)
+    class Meta:
+        model = models.Details
+        fields = ('id', 'job_name', 'time_from', 'time_to', 'duration', 'pay_per_hour', 'tags', 'address', 'description', 'required_experience')
+
+    def create(self, validated_data):
+        tags_data = validated_data.pop('tags')
+        details = models.Details.objects.create(**validated_data)
+        for obj in tags_data:
+            tag = models.Tag.objects.filter(text=obj.tag)
+            if tag.exists():
+                new_tag = tag.first()
+            else:
+                new_tag = Tag.objects.get(**obj)
+                tags.add(new_tag)
+        return details
+
 class JobsCreationSerializer(serializers.ModelSerializer):
+    details = DetailsSerializer(many=False)
     class Meta:
         model = models.Job
-        fields = ('id', 'name')
+        fields = ('id', 'employer_id', 'details')
+
+    def create(self, validated_data):
+        details_data = validated_data.pop('details')
+        job = models.Job.objects.create(**validated_data)
+        models.Details.objects.create(**details_data)
+        return job
